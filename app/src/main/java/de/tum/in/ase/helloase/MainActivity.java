a717355061308ccf0233d0d2eaf3f99b2c664a7f
Benedikt Schlagberger <schlagbe@in.tum.de>
2016-12-04 17:18:27 +0100

    added menu

--- app/src/main/java/de/tum/in/ase/helloase/MainActivity.java
+++ app/src/main/java/de/tum/in/ase/helloase/MainActivity.java
@@ -2,6 +2,9 @@ package de.tum.in.ase.helloase;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.ase_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.show_ase:
+                TextView textView = (TextView) findViewById(R.id.text_view);
+                textView.setText("Hello ASE!");
+                return true;
+            default:
+                return false;
+        }
     }
 }
